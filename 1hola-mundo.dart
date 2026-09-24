c1fe843f74a29a907fa6fd01a451964d3e6e27eb
Francisco Herrera <fhercar3008@g.educaand.es>
2026-09-24 19:36:33 +0200

    Subido ya porfin

---
+++ 1hola-mundo.dart
@@ -0,0 +1,15 @@
+/* Ejercicio 1 de la relacion 1
+    archivo: 1hol-mundo.dart
+    fecha creación: septiembre 2026
+    autor: Francisco Herrera`*/
+
+void main(){
+
+    String nombre = 'Fran'; // tipo y nombre de la variable
+    int veces = 1;
+    print ('Hola $nombre!'); // la variable con dolar delante dentro del print muestra su valor
+    print ('Has ejecutado el programa ${veces*2-1} vez(es)'); // si es una expresion se pone entre
+}
+
+
+
